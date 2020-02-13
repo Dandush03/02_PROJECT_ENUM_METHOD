447eb3c0b347bab584320234056f6e0a977fd40f
Dandush03 <d.laloush@outlook.com>
2020-02-13 18:08:17 -0400

    Added Stickler Config and Second Function

--- main.rb
+++ main.rb
@@ -7,7 +7,6 @@ module Enumerable
       while min < max do
         puts min
         min += 1
-      
       end
     else
       min = 0
@@ -18,9 +17,31 @@ module Enumerable
       end
     end
   end
+
+  def my_each_with_idex
+    if self.is_a? Range then
+      min = self.min
+      max = self.max
+
+      while min < max do
+        puts min
+        min += 1
+      end
+    else
+      min = 0
+      max = length
+      while min < max
+        yield(self[min], min)
+        min += 1
+    end
+  end
+
 end
 temp_array = %w[cat dog fish turtle]
 range = (2..4)
-#puts range.each { |n| puts n }
-puts temp_array.my_each { |n| puts n }
+puts ' ############### my_each ############### '
+puts range.each { |n| puts n }
 puts range.my_each { |n| puts n }
+puts ' ############### my_each ############### '
+puts temp_array.each { |n| puts n }
+puts temp_array.my_each { |n| puts n }
