babbd4992be0b62d53b7735d8a20fd293a42b178
Dandush03 <d.laloush@outlook.com>
2020-02-17 13:31:21 -0400

    Fixed Bugs in my_any? && Fixed Bugs in my_all?

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+# rubocop: disable Metrics/ModuleLength
+# rubocop: disable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
 module Enumerable
   def my_each
     if is_a? Range
@@ -17,6 +19,7 @@ module Enumerable
         yield(self[min])
         min += 1
       end
+      self
     else
       to_enum(:my_each)
     end
@@ -41,6 +44,7 @@ module Enumerable
         yield(self[min], min)
         min += 1
       end
+      self
     else
       to_enum(:my_each_with_index)
     end
@@ -58,29 +62,34 @@ module Enumerable
     end
   end
 
-  def my_all?
+  def my_all?(opt = nil)
     bool_item = true
-    if block_given?
-      my_each do |item|
-        bool_item = false unless yield(item)
-      end
+    if opt.nil? && block_given?
+      my_each { |item| bool_item = false unless yield(item) }
+    elsif opt.is_a?(Regexp)
+      my_each { |item| bool_item = false if item.match(opt) }
+    elsif opt.is_a?(Module)
+      my_each { |item| bool_item = false unless item.is_a?(opt) }
+    else
+      my_each { |item| bool_item = false if item.nil? || item == false }
     end
     bool_item
   end
 
-  def my_any?
-    bool_item = true
-    if block_given?
-      bool_item = false
-      my_each do |item|
-        bool_item = true if yield(item)
-      end
+  def my_any?(opt = nil)
+    bool_item = false
+    if opt.nil? && block_given?
+      my_each { |item| bool_item = true if yield(item) }
+    elsif opt.is_a?(Regexp)
+      my_each { |item| bool_item = true unless item.match(opt) }
+    elsif opt.is_a?(Module)
+      my_each { |item| bool_item = true if item.is_a?(opt) }
+    else
+      my_each { |item| bool_item = true unless item.nil? || item == false }
     end
     bool_item
   end
-end
 
-module Enumerable
   def my_none?
     if block_given?
       bool_item = true
@@ -118,149 +127,27 @@ module Enumerable
   end
 
   def my_inject(temp_option = nil)
-    new_items = temp_option || shift
-    my_each do |item|
-      new_items = yield(new_items, item)
+    if is_a? Range
+      min = self.min
+      max = self.max
+      new_items = temp_option || 0
+      while min <= max
+        new_items = yield(new_items, min)
+        min += 1
+      end
+    else
+      new_items = temp_option || shift
+      my_each do |item|
+        new_items = yield(new_items, item)
+      end
     end
     new_items
   end
 end
 
+# rubocop: enable Metrics/ModuleLength
+# rubocop: enable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
+
 def multiply_els(array_num)
   array_num.my_inject { |results, number| results * number }
 end
-
-temp_array = %w[cat dog fish turtle]
-range = (2..4)
-temp_number = [1, 2, 3, 4, 5, 6]
-puts ' '
-puts ' ####################################### '
-puts ' ############### my_each ############### '
-puts ' ####################################### '
-puts ' '
-puts ' ############# Range Type ############## '
-range.each { |n| puts n }
-puts ' '
-range.my_each { |n| puts n }
-puts ' '
-puts ' ############# Array Type ############## '
-temp_array.each { |n| puts n }
-puts ' '
-temp_array.my_each { |n| puts n }
-puts ' '
-puts ' ####################################### '
-puts ' ######### my_each_with_index ########## '
-puts ' ####################################### '
-puts ' '
-puts ' ############# Range Type ############## '
-range.each_with_index { |n, m| puts "#{n}:#{m}" }
-puts ' '
-range.my_each_with_index { |n, m| puts "#{n}:#{m}" }
-puts ' '
-puts ' ############# Array Type ############## '
-temp_array.each_with_index { |n, m| puts "#{n}:#{m}" }
-puts ' '
-temp_array.my_each_with_index { |n, m| puts "#{n}:#{m}" }
-puts ' '
-puts ' ####################################### '
-puts ' ############## my_select ############## '
-puts ' ####################################### '
-puts ' '
-puts temp_number.select(&:even?)
-puts ' '
-puts temp_number.my_select(&:even?)
-puts ' '
-puts ' ####################################### '
-puts ' ############### my_all ################ '
-puts ' ####################################### '
-puts ' '
-temp = temp_array.all? { |word| word.length >= 3 }
-puts temp
-puts ' '
-temp = temp_array.my_all? { |word| word.length >= 3 }
-puts temp
-puts ' '
-puts ' ####################################### '
-puts ' ############## my_any? ################ '
-puts ' ####################################### '
-puts ' '
-temp = temp_array.any? { |word| word.length >= 7 }
-puts temp
-puts ' '
-temp = temp_array.my_any? { |word| word.length >= 7 }
-puts temp
-puts ' '
-puts ' ####################################### '
-puts ' ############## my_none? ############### '
-puts ' ####################################### '
-puts ' '
-temp = temp_array.none? { |word| word.length >= 7 }
-puts temp
-puts ' '
-temp = temp_array.my_none? { |word| word.length >= 7 }
-puts temp
-puts ' '
-puts ' ####################################### '
-puts ' ############## my_count ############### '
-puts ' ####################################### '
-puts ' '
-temp = temp_number.count { |num| (num % 3).zero? }
-puts temp
-puts ' '
-temp = temp_number.my_count { |num| (num % 3).zero? }
-puts temp
-puts ' '
-puts ' ####################################### '
-puts ' ############### my_map ################ '
-puts ' ####################################### '
-puts ' '
-temp = temp_number.map { |num| num * 2 }
-puts temp
-puts ' '
-temp = temp_number.my_map { |num| num * 2 }
-puts temp
-puts ' '
-puts ' ####################################### '
-puts ' ############# my_inject ############### '
-puts ' ####################################### '
-puts ' '
-temp = temp_number.inject(0) { |results, elements| results + elements }
-puts temp
-puts ' '
-temp = temp_number.my_inject(0) { |results, elements| results + elements }
-puts temp
-puts ' '
-puts ' ####################################### '
-puts ' ############ multiply_els ############# '
-puts ' ####################################### '
-puts ' '
-temp = multiply_els(temp_number)
-puts temp
-puts ' '
-puts ' ####################################### '
-puts ' ########### proc challenge ############ '
-puts ' ####################################### '
-puts ' '
-temp = proc { |number| number * 4 }
-puts temp_number.my_map(temp)
-puts ' '
-temp = temp_number.my_map { |number| number * 2 }
-puts temp
-puts '#########################################'
-puts temp_number.each
-puts temp_number.my_each
-puts temp_number.my_each
-puts temp_number.each_with_index
-puts temp_number.my_each_with_index
-puts temp_number.select
-puts temp_number.my_select
-puts temp_number.all?
-puts temp_number.my_all?
-puts temp_number.any?
-puts temp_number.my_any?
-puts temp_number.none?
-puts temp_number.my_none?
-puts temp_number.count
-puts temp_number.my_count
-puts temp_number.map
-puts temp_number.my_map
